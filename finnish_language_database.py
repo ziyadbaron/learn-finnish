1d0c15ffa25be43182528a91c6a4c4bdd3741a7e
ziyad <zyadbaron@gmail.com>
2022-08-22 22:00:44 +0300

    Change customer ID to user id

--- finnish_language_database.py
+++ finnish_language_database.py
@@ -18,14 +18,14 @@ def connect():
     return sqlite3.connect("db_finish_language.db")
 
 
-def create_customer_table(connection, customer_ID):
+def create_customer_table(connection, user_id):
     with connection:
-        connection.execute(CREATE_CUSTOMER_TABLE.format(customer_ID))
+        connection.execute(CREATE_CUSTOMER_TABLE.format(user_id))
 
 
-def check_if_table_is_empty(connection, customer_ID):
+def check_if_table_is_empty(connection, user_id):
     with connection:
-        return connection.execute(CHECK_IF_TABLE_IS_EMPTY.format(customer_ID)).fetchall()
+        return connection.execute(CHECK_IF_TABLE_IS_EMPTY.format(user_id)).fetchall()
 
 
 def choose_new_words(connection, rows_number, begin_from):
@@ -33,22 +33,22 @@ def choose_new_words(connection, rows_number, begin_from):
         return connection.execute(CHOOSE_NEW_WORDS, ((rows_number), (begin_from))).fetchall()
 
 
-def insert_word_to_user(connection, customer_ID, word_ID):
+def insert_word_to_user(connection, user_id, word_ID):
     with connection:
         connection.execute(INSERT_WORD_TO_USER.format(
-            customer_ID), (word_ID, 1, 0))
+            user_id), (word_ID, 1, 0))
 
 
-def get_the_last_row(connection, customer_ID):
+def get_the_last_row(connection, user_id):
     with connection:
-        return connection.execute(GET_THE_LAST_ROW.format(customer_ID,  customer_ID)).fetchone()
+        return connection.execute(GET_THE_LAST_ROW.format(user_id,  user_id)).fetchone()
 
 
-def choose_random_user_words(connection, customer_ID, Number_of_words):
+def choose_random_user_words(connection, user_id, Number_of_words):
     with connection:
         words_list = []
         random_ids = connection.execute(CHOOSE_USER_RANDOM_WORDS_ID.format(
-            customer_ID, Number_of_words)).fetchall()
+            user_id, Number_of_words)).fetchall()
         for id in random_ids:
             fin, eng = connection.execute(GET_WORDS, (id)).fetchone()
             words_list.append((id, fin, eng))
